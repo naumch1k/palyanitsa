62fb54db2fc2c611cf929481c7b4ee0d925dcf50
Irina Naumchik <irina.naumchik2012@yandex.ru>
2022-05-06 17:51:47 -0500

    chore: add fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { App } from '../components/app';
 
+import '../shared/styles/fonts.css';
 import '../shared/styles/palette.css';
 import '../shared/styles/vars.css';
 import '../shared/styles/globals.css';
