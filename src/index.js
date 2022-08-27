be3c2b0f908fc3a3ca4992b41f5fe8a0a5a55206
Irina Naumchik <irina.naumchik2012@yandex.ru>
2022-08-26 21:38:32 -0500

    chore: add fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './shared/styles/global.css';
+import './index.css';
 import App from './components/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
