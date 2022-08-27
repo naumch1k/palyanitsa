3990f706f410870fc81ec350b5766366ae01f4e6
Irina Naumchik <irina.naumchik2012@yandex.ru>
2022-08-26 21:00:59 -0500

    chore: install sass, adjust script

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,7 +1,9 @@
-const Home = () => (
-  <h1>
+import styles from './app.module.scss';
+
+const App = () => (
+  <h1 className={styles.title}>
     Hello, world!
   </h1>
 );
 
-export default Home;
+export default App;
