dab98c7ad99285e1ab4e15586efb41d21c61b10e
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-06-16 15:48:02 -0500

    feat: add donations block mosk data and images

---
+++ src/components/app/assets/homepage-donations/mock-data.js
@@ -0,0 +1,30 @@
+import HeadlineImage from './headline-image.jpg';
+import VenmoQRCode from './venmo-qr-code.png';
+
+export const donationsData = {
+  tag: '04',
+  title: 'Donate',
+  subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing dolor sit amet',
+  headline_image: HeadlineImage,
+  donation_platforms: [
+    {
+      name: 'Paypal',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
+      link: 'https://www.paypal.com/us/home',
+      linkText: 'Donate via PayPal website',
+    },
+    {
+      name: 'Venmo',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
+      link: 'https://venmo.com/',
+      linkText: 'Donate via Venmo app',
+      qrCode: VenmoQRCode,
+    },
+    {
+      name: 'Zelle',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
+      link: '',
+      linkText: 'Donate via Zelle website',
+    }
+  ]
+};
