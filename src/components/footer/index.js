1db8d7a8eb30390a57ffa10917ab42f5f6ac2493
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-06-21 11:54:55 -0500

    feat: added footer social links, restyled copyright

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,9 +1,19 @@
 import { TextLink } from '../ui/text-link';
 import styles from './footer.module.scss';
+import footerSocialLinks from '../../utils/footer-social-links';
 
 export const Footer = () => {
   return (
     <div className={styles.root}>
+      <ul className={styles.socialLinks}>
+        {footerSocialLinks.map(link => (
+          <li key={link.id}>
+            <a className={styles.socialLink} href={link.href} target="_blank" rel="noreferrer">
+              <link.icon/>
+            </a>
+          </li>
+        ))}
+      </ul>
       <div className={styles.copyright}>
         <p className={styles.text}>built by {' '}
           <TextLink
---
+++ src/utils/footer-social-links.js
@@ -0,0 +1,22 @@
+import { FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { RiPatreonFill } from 'react-icons/ri';
+
+const footerSocialLinks = [
+  {
+    id: 'instagram',
+    href: 'https://www.instagram.com/',
+    icon: FaInstagram,
+  },
+  {
+    id: 'linkedin',
+    href: 'https://www.linkedin.com/',
+    icon: FaLinkedin,
+  },
+  {
+    id: 'patreon',
+    href: 'https://www.patreon.com/',
+    icon: RiPatreonFill,
+  },
+];
+
+export default footerSocialLinks;
