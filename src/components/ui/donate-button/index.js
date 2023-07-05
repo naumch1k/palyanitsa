c12eaac9e6f714ccd26d0370066f2598abc521b3
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-07-05 15:27:38 -0500

    refactor: restyle donate button

--- src/components/ui/donate-button/index.js
+++ src/components/ui/donate-button/index.js
@@ -1,46 +1,6 @@
-import { useState, useEffect } from 'react';
 import styles from './donate-button.module.scss';
 
 export const DonateButton = () => {
-  const [isHovered, setIsHovered] = useState(false);
-  const [buttonText, setButtonText] = useState('');
-
-  useEffect(() => {
-    let typingInterval;
-
-    if (window.innerWidth > 1000) {
-      if (isHovered) {
-        const text = 'Donate';
-        let currentIndex = 0;
-
-        typingInterval = setInterval(() => {
-          setButtonText(prevText => {
-            if (currentIndex < text.length) {
-              return prevText + text[currentIndex];
-            } else {
-              return prevText;
-            }
-          });
-
-          currentIndex++;
-
-          if (currentIndex > text.length) {
-            clearInterval(typingInterval);
-          }
-        }, 120);
-      } else {
-        setButtonText('D');
-      }
-    } else {
-      setButtonText('D');
-    }
-
-    return () => clearInterval(typingInterval);
-  }, [isHovered]);
-
-  const handleMouseEnter = () => setIsHovered(true);
-  const handleMouseLeave = () => setIsHovered(false);
-
   const handleClick = () => {
     const element = document.querySelector('#donations-anchor');
 
@@ -48,13 +8,8 @@ export const DonateButton = () => {
   };
 
   return (
-    <button
-      className={styles.root}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      onClick={handleClick}
-    >
-      {buttonText}
+    <button className={styles.root} onClick={handleClick}>
+      <span>Donate</span>
     </button>
   );
 };
