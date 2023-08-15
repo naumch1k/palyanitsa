a6c50668da5ab2fbd9457a7c99e8e55ff76b4107
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-08-15 16:11:32 -0500

    feat: set up routing

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,78 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
 import { AppLayout } from '../app-layout';
-import { HomepageLayout } from '../homepage-layout';
-import { HomepageHeadline } from '../homepage-headline';
-import { HomepageAbout } from '../homepage-about';
-import { HomepageDisciplines } from '../homepage-disciplines';
-import { HomepageTeam } from '../homepage-team';
-import { HomepageDonations } from '../homepage-donations';
-import { HomepageCallToAction } from '../homepage-call-to-action';
-import { HomepagePartners } from '../homepage-partners';
-
-import { aboutData } from './assets/homepage-about/mock-data.js';
-import { disciplinesData } from './assets/homepage-disciplines/mock-data';
-import { teamData } from './assets/homepage-team/mock-data.js';
-import { donationsData } from './assets/homepage-donations/mock-data.js';
-import { callToActionData } from './assets/homepage-call-to-action/mock-data';
-import { partnersData } from './assets/homepage-partners/mock-data';
+import Home from '../../pages/home';
 
 const App = () => (
   <AppLayout>
-    <HomepageLayout>
-      <HomepageLayout.Headline>
-        <HomepageHeadline/>
-      </HomepageLayout.Headline>
-      <HomepageLayout.About>
-        <HomepageAbout
-          tag={aboutData.tag}
-          title={aboutData.title}
-          subtitle={aboutData.subtitle}
-          headline_image={aboutData.headline_image}
-          principles={aboutData.principles}
-          quotes={aboutData.quotes}
-        />
-      </HomepageLayout.About>
-      <HomepageLayout.Disciplines>
-        <HomepageDisciplines
-          tag={disciplinesData.tag}
-          title={disciplinesData.title}
-          subtitle={disciplinesData.subtitle}
-          headline_image={disciplinesData.headline_image}
-          disciplines={disciplinesData.disciplines}
-        />
-      </HomepageLayout.Disciplines>
-      <HomepageLayout.Team>
-        <HomepageTeam
-          tag={teamData.tag}
-          title={teamData.title}
-          subtitle={teamData.subtitle}
-          headline_image={teamData.headline_image}
-          team={teamData.team}
-        />
-      </HomepageLayout.Team>
-      <HomepageLayout.Donations>
-        <HomepageDonations
-          tag={donationsData.tag}
-          title={donationsData.title}
-          subtitle={donationsData.subtitle}
-          headline_image={donationsData.headline_image}
-          donation_platforms={donationsData.donation_platforms}
-        />
-      </HomepageLayout.Donations>
-      <HomepageLayout.CallToAction>
-        <HomepageCallToAction
-          tag={callToActionData.tag}
-          title={callToActionData.title}
-          subtitle={callToActionData.subtitle}
-          headline_image={callToActionData.headline_image}
-          general_info={callToActionData.general_info}
-        />
-      </HomepageLayout.CallToAction>
-      <HomepageLayout.Partners>
-        <HomepagePartners
-          partners={partnersData}
-        />
-      </HomepageLayout.Partners>
-    </HomepageLayout>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
   </AppLayout>
 );
 
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,78 @@
+import { HomepageLayout } from '../../components/homepage-layout';
+import { HomepageHeadline } from '../../components/homepage-headline';
+import { HomepageAbout } from '../../components/homepage-about';
+import { HomepageDisciplines } from '../../components/homepage-disciplines';
+import { HomepageTeam } from '../../components/homepage-team';
+import { HomepageDonations } from '../../components/homepage-donations';
+import { HomepageCallToAction } from '../../components/homepage-call-to-action';
+import { HomepagePartners } from '../../components/homepage-partners';
+
+import { aboutData } from './assets/homepage-about/mock-data.js';
+import { disciplinesData } from './assets/homepage-disciplines/mock-data';
+import { teamData } from './assets/homepage-team/mock-data.js';
+import { donationsData } from './assets/homepage-donations/mock-data.js';
+import { callToActionData } from './assets/homepage-call-to-action/mock-data';
+import { partnersData } from './assets/homepage-partners/mock-data';
+
+const Home = () => {
+  return (
+    <HomepageLayout>
+      <HomepageLayout.Headline>
+        <HomepageHeadline/>
+      </HomepageLayout.Headline>
+      <HomepageLayout.About>
+        <HomepageAbout
+          tag={aboutData.tag}
+          title={aboutData.title}
+          subtitle={aboutData.subtitle}
+          headline_image={aboutData.headline_image}
+          principles={aboutData.principles}
+          quotes={aboutData.quotes}
+        />
+      </HomepageLayout.About>
+      <HomepageLayout.Disciplines>
+        <HomepageDisciplines
+          tag={disciplinesData.tag}
+          title={disciplinesData.title}
+          subtitle={disciplinesData.subtitle}
+          headline_image={disciplinesData.headline_image}
+          disciplines={disciplinesData.disciplines}
+        />
+      </HomepageLayout.Disciplines>
+      <HomepageLayout.Team>
+        <HomepageTeam
+          tag={teamData.tag}
+          title={teamData.title}
+          subtitle={teamData.subtitle}
+          headline_image={teamData.headline_image}
+          team={teamData.team}
+        />
+      </HomepageLayout.Team>
+      <HomepageLayout.Donations>
+        <HomepageDonations
+          tag={donationsData.tag}
+          title={donationsData.title}
+          subtitle={donationsData.subtitle}
+          headline_image={donationsData.headline_image}
+          donation_platforms={donationsData.donation_platforms}
+        />
+      </HomepageLayout.Donations>
+      <HomepageLayout.CallToAction>
+        <HomepageCallToAction
+          tag={callToActionData.tag}
+          title={callToActionData.title}
+          subtitle={callToActionData.subtitle}
+          headline_image={callToActionData.headline_image}
+          general_info={callToActionData.general_info}
+        />
+      </HomepageLayout.CallToAction>
+      <HomepageLayout.Partners>
+        <HomepagePartners
+          partners={partnersData}
+        />
+      </HomepageLayout.Partners>
+    </HomepageLayout>
+  );
+};
+
+export default Home;
