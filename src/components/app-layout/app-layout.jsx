36af75abd39630eebe66667c404624e5ed0e1c12
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-08-17 12:50:28 -0500

    feat: implemented app navigation menu

--- src/components/app-layout/app-layout.jsx
+++ src/components/app-layout/app-layout.jsx
@@ -1,9 +1,18 @@
+import { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { Page } from '../page';
+import Page from '../page';
 import { Footer } from '../footer';
+import { BurgerButton } from '../ui/burger-button';
 import { DonateButton } from '../ui/donate-button';
+import { NavigationMenu } from '../navigation-menu';
 
 export const AppLayout = ({ children }) => {
+  const [isOverlayMenuOpen, setIsOverlayMenuOpen] = useState(false);
+
+  const toggleOverlayMenu = useCallback(() => {
+    setIsOverlayMenuOpen(!isOverlayMenuOpen);
+  }, [isOverlayMenuOpen]);
+
   return (
     <Page>
       <Page.Header/>
@@ -13,9 +22,18 @@ export const AppLayout = ({ children }) => {
       <Page.Footer>
         <Footer/>
       </Page.Footer>
+      <Page.BurgerButton>
+        <BurgerButton
+          isOpen={isOverlayMenuOpen}
+          onClick={toggleOverlayMenu}
+        />
+      </Page.BurgerButton>
       <Page.DonateButton>
         <DonateButton/>
       </Page.DonateButton>
+      <Page.OverlayMenu isOpen={isOverlayMenuOpen}>
+        <NavigationMenu/>
+      </Page.OverlayMenu>
     </Page>
   );
 };
