ba5a09eff86d5ea853727c445da492d9e4962e60
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-08-23 19:21:58 -0500

    feat: added PageTitle component, implemented it to news page

--- src/components/news-layout/index.js
+++ src/components/news-layout/index.js
@@ -1,3 +1,8 @@
-import { NewsLayout } from './news-layout';
+import { NewsLayout as Component } from './news-layout';
+import { NewsLayoutTitle } from './title';
+
+const NewsLayout = Object.assign(Component, {
+  Title: NewsLayoutTitle,
+});
 
 export default NewsLayout;
--- src/pages/news/index.jsx
+++ src/pages/news/index.jsx
@@ -1,6 +1,7 @@
 import { AppLayout } from '../../components/app-layout';
 import NewsLayout from '../../components/news-layout';
 import NewsList from '../../components/news-list';
+import { PageTitle } from '../../components/page-title';
 import { NewsCard } from '../../components/news-card/news-card';
 
 import { mockData } from './assets/mock-data';
@@ -9,6 +10,9 @@ const News = () => {
   return (
     <AppLayout>
       <NewsLayout>
+        <NewsLayout.Title>
+          <PageTitle text="The latest"/>
+        </NewsLayout.Title>
         <NewsList>
           {mockData.news.map((item, i) => (
             <NewsList.Item key={i}>
