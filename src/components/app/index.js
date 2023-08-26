60d4df967476648a8997ab1ae6ed34c4e9416328
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-08-26 14:03:34 -0500

    feat: implemented ScrollToTop component for automatic scroll restoration

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,14 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
+import ScrollToTop from '../scroll-to-top';
 import Home from '../../pages/home';
 import News from '../../pages/news';
 import PageNotFound from '../../pages/404';
 
 const App = () => (
-  <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/news" element={<News/>}/>
-    <Route path="*" element={<PageNotFound/>}/>
-  </Routes>
+  <ScrollToTop>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/news" element={<News/>}/>
+      <Route path="*" element={<PageNotFound/>}/>
+    </Routes>
+  </ScrollToTop>
 );
 
 export default App;
