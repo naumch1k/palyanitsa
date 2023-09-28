ac0c55caa746e8a48e5c549f477586410d6410cf
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-09-28 15:34:49 -0500

    updated other ways to help info

--- src/shared/constants/other-ways-to-help.js
+++ src/shared/constants/other-ways-to-help.js
@@ -6,6 +6,9 @@ const otherWaysToHelp = [
       From one-time options to ongoing collaborations, your involvement
       on behalf of children can create a ripple effect that changes their
       lives and the future we share.`,
+      `If you have expertise in IT fields, become a mentor and impart valuable skills.
+      Your guidance not only enhances their technical abilities but also offers
+      much-needed emotional support.`,
       'Drop us a line at hello@pgds.xyz',
       'We\'re always here and ready to chat!',
     ],
