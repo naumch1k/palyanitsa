1d43d05c3fde18b81e11a922f27be468ef363563
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-10-03 13:44:05 -0500

    feat: replaced SliderDots with ArrowButtons for blog's and stories' carousels

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import { SliderArrowButton } from '../ui/slider-arrow-button';
-import { SliderDots } from '../ui/slider-dots/slider-dots';
 import useCarousel from '../../hooks/use-carousel';
 import styles from './carousel.module.scss';
 
@@ -17,7 +16,6 @@ export const Carousel = ({ numberOfCenteredSlides = 2, children }) => {
 
   const handleLeftArrowClick = evt => evt.stopPropagation() || instanceRef.current?.prev();
   const handleRightArrowClick = evt => evt.stopPropagation() || instanceRef.current?.next();
-  const handleSliderDotClick = i => instanceRef.current?.moveToIdx(i);
 
   return (
     <>
@@ -29,26 +27,16 @@ export const Carousel = ({ numberOfCenteredSlides = 2, children }) => {
       </ul>
       {loaded && instanceRef.current && (
         <div className={styles.sliderControls}>
-          {window.innerWidth >= 768 ? (
-            <>
-              <SliderArrowButton
-                direction="left"
-                onClick={handleLeftArrowClick}
-                disabled={currentSlide === 0}
-              />
-              <SliderArrowButton
-                direction="right"
-                onClick={handleRightArrowClick}
-                disabled={currentSlide === children.length - 1}
-              />
-            </>
-          ) : (
-            <SliderDots
-              count={children.length}
-              currentSlide={currentSlide}
-              onSliderDotClick={handleSliderDotClick}
-            />
-          )}
+          <SliderArrowButton
+            direction="left"
+            onClick={handleLeftArrowClick}
+            disabled={currentSlide === 0}
+          />
+          <SliderArrowButton
+            direction="right"
+            onClick={handleRightArrowClick}
+            disabled={currentSlide === children.length - 1}
+          />
         </div>
       )}
     </>
