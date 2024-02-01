114046436b6409f2ed10b3d8cca478996b4efdd8
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-01-31 20:44:32 -0600

    feat: adjusted blog entry's image styles for better responsiveness

--- src/components/blog-entry-card/blog-entry-card.jsx
+++ src/components/blog-entry-card/blog-entry-card.jsx
@@ -7,6 +7,8 @@ export const BlogEntryCard = ({ data }) => {
     id,
     date,
     heading,
+    image_desktop,
+    image_desktop_webp,
     image_tablet,
     image_tablet_webp,
     image_mobile,
@@ -16,9 +18,11 @@ export const BlogEntryCard = ({ data }) => {
   return (
     <Link className={styles.link} to={`${id}`} aria-label={`Read: ${heading}`}>
       <picture className={styles.imageWrapper}>
+        <source type="image/webp" media="(min-width: 768px)" srcSet={image_desktop_webp}/>
         <source type="image/webp" media="(min-width: 428px)" srcSet={image_tablet_webp}/>
         <source type="image/webp" srcSet={image_mobile_webp}/>
 
+        <source media="(min-width: 768px)" srcSet={image_desktop}/>
         <source media="(min-width: 428px)" srcSet={image_tablet}/>
         <img className={styles.image} src={image_mobile} alt={''}/>
       </picture>
@@ -36,6 +40,8 @@ BlogEntryCard.propTypes = {
     id: PropTypes.string.isRequired,
     date: PropTypes.string,
     heading: PropTypes.string.isRequired,
+    image_desktop: PropTypes.string.isRequired,
+    image_desktop_webp: PropTypes.string,
     image_tablet: PropTypes.string.isRequired,
     image_tablet_webp: PropTypes.string,
     image_mobile: PropTypes.string,
